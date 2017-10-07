3862557491bcfb0d71cfe656b9d4ab92e3993ed9
Vibhanshu Bhardwaj <bhardwajvibhanshu@gmail.com>
2017-10-07 14:13:52 -0400

    tried making an abstraction, didn't work.

--- A4/Analysis.scala
+++ A4/Analysis.scala
@@ -7,34 +7,35 @@ object Analysis {
 		return ret;
 	}
 
-	def getAllPossibleCategories() : Seq[String] = {
+	def getAllPossibleFamilies() : Seq[String] = {
 		var categories = Seq[String]();
 		categories = categories :+ "WORD"
 		categories = categories :+ "LABEL"
 		return categories
 	}
 
-	def getTokenSequencesOfCategory(category: String) : Seq[Seq[String]] = {
+	def getTokenSequencesOfFamily(family: String) : Seq[Seq[String]] = { //correct token sequence of what FOLLOWS the family
+		//for eg, for the family JUMP, the only correct sequence is "REG". so we'll output [["REG"]]
 		var seqOfCorrectSeq = Seq[Seq[String]]()
 
-		if (category == "NOCATEGORY") {
+		if (family == "NOCATEGORY") {
 			println("tried to get valid token sequences of NOCATEGORY. Shouldn't happpen")
 			return seqOfCorrectSeq;
 		}
 
-		else if (category == "WORD") {
+		else if (family == "WORD") {
 			var correctSequence1 = Seq[String]();
-			correctSequence1 = appendToSequence(correctSequence1, "WORD");
+			//correctSequence1 = appendToSequence(correctSequence1, "WORD");
 			correctSequence1 = appendToSequence(correctSequence1, "INT");
 			seqOfCorrectSeq = seqOfCorrectSeq :+ correctSequence1
 
 			var correctSequence2 = Seq[String]();
-			correctSequence2 = appendToSequence(correctSequence2, "WORD");
+			//correctSequence2 = appendToSequence(correctSequence2, "WORD");
 			correctSequence2 = appendToSequence(correctSequence2, "HEXINT");
 			seqOfCorrectSeq = seqOfCorrectSeq :+ correctSequence2
 
 			var correctSequence3 = Seq[String]();
-			correctSequence3 = appendToSequence(correctSequence3, "WORD");
+			//correctSequence3 = appendToSequence(correctSequence3, "WORD");
 
 			//for the instruction .word <LABEL_NAME>. The token's kind is ID
 			correctSequence3 = appendToSequence(correctSequence3, "ID"); 
@@ -48,21 +49,21 @@ object Analysis {
 		//so we just don't start our correct sequence with arbitrary no. of labels. we simply ignore and check if
 		// what follows label is a correct sequence, and if it is the entire sequence is correct.
 
-		else if (category == "LABEL") { 
+		else if (family == "LABEL") { 
 			var correctSequence1 = Seq[String]();
 			seqOfCorrectSeq = seqOfCorrectSeq :+ correctSequence1;
 
 			var correctSequence2 = Seq[String]();
-			val allPossibleCategories : Seq[String] = getAllPossibleCategories();
+			val allPossibleFamilies : Seq[String] = getAllPossibleFamilies();
 			var allCorrectSequences : Seq[Seq[String]] = Seq[Seq[String]]();
 
 			//for an instruction starting with LABEL, sequences of correct tokens are all possible correct sequences
 			// of all instructions because a label can be followed by any instruction.
 
-			for (c <- allPossibleCategories) {
+			for (c <- allPossibleFamilies) {
 				//checked for not LABEL to prevent infinite recursion	
 				if (c != "LABEL") {
-					allCorrectSequences = allCorrectSequences ++ getTokenSequencesOfCategory(c)
+					allCorrectSequences = allCorrectSequences ++ getTokenSequencesOfFamily(c)
 				}
 			}
 
@@ -74,7 +75,7 @@ object Analysis {
 		return seqOfCorrectSeq;
 	}
 
-	def getCategoryOfToken(myToken: Token) : String = {
+	def getFamilyOfToken(myToken: Token) : String = {
 
 		if (myToken.kind == "WORD") return "WORD";
 		else if (myToken.kind == "LABEL") return "LABEL"
@@ -82,11 +83,24 @@ object Analysis {
 	}
 
 	def getCorrectTokenKindSequences(firstToken: Token) : Seq[Seq[String]] = {
-		var category = getCategoryOfToken(firstToken); // is it one of (add, sub, slt, sltu) or one of other 6 types
-		var correctTokenSequences = getTokenSequencesOfCategory(category)
+		var family = getFamilyOfToken(firstToken); // is it one of (add, sub, slt, sltu) or one of other 6 types
+		var correctTokenSequences = getTokenSequencesOfFamily(family)
 		return correctTokenSequences;
 	}
 
+	def prepend(seq: Seq[String], s: String) : Seq[String] = {
+		println("prepend not working")
+		var ret = s +: seq;
+		return ret;
+	}
+	def getCompleteCorrectTokenSequence(token: Token) : Seq[Seq[String]] {
+		var validTokenSequencesOfFamily = getTokenSequencesOfFamily(getFamilyOfToken(token));
+		if (token.kind == "LABEL") return validTokenSequencesOfFamily;
+		else {
+			var ret = validTokenSequencesOfFamily;
+			ret = validTokenSequencesOfFamily.map(x => prepend(x, token.kind));
+		}
+	}
 	def isTokenLineCorrect(tokenLine: Seq[Token]) : Boolean  = {
 		//check if the line is a correct instruction. if empty, it's correct
 
@@ -95,17 +109,30 @@ object Analysis {
 		}
 		var firstToken = tokenLine.apply(0);
 
-		var correctTokenSequences = getCorrectTokenKindSequences(firstToken);
+		var correctTokenSequences = getCorrectTokenKindSequences(firstToken); //THIS DOES NOT INCLUDE THE KIND OF firstToken.
+		println("correctTokenSequences: ")
+		correctTokenSequences.map(x => print(x + ", "))
+		println("")
+		var completeCorrectTokenSequence = correctTokenSequences.map(x => {
+			print("firstToken.kind " + firstToken.kind)
+			if (firstToken.kind != "LABEL") prepend(x, firstToken.kind);
+			else x
+		})  //complete cuz it includes the kind of firstToken
+		println("completeCorrectTokenSequence: ")
+		completeCorrectTokenSequence.map(x => print(x + ", "))
+		println("")
 		//check if the given sequence of kind of tokens is one of correctTokenSequences
 
 		var seqOfKinds = Seq[String]();
-
+		//constructing the sequence of kinds of token for GIVEN instructions
 		for (i <- 0 until tokenLine.length) {
 			var token = tokenLine.apply(i);
 			//we're ignoring the labels at the start of instructions (ie definitions) because there can be arbitrary no. of them
 			if (token.kind != "LABEL" || seqOfKinds.length > 0 ) seqOfKinds = appendToSequence(seqOfKinds, token.kind);
 		}
-		
+		println("seqOfKinds: ")
+		seqOfKinds.map(x => print(x + ", "))
+		println("")
 		return correctTokenSequences.contains(seqOfKinds);
 	}
 }
