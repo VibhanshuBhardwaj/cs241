73760c13a5813a90c42b62c5035ddfc6e97a0690
Vibhanshu Bhardwaj <bhardwajvibhanshu@gmail.com>
2017-11-20 13:56:36 -0500

    started A9

--- A9/P1/WLP4Gen.scala
+++ A9/P1/WLP4Gen.scala
@@ -16,7 +16,14 @@ object WLP4Gen {
 
 	type FunctionSymTable = (String, Map[String, String], Int);
 
-
+	def generateCode(proceduresTree: Node) : Unit = {
+		val children = proceduresTree.children;
+		val mainTree = children(0);
+		val expr = children(11);
+		val id = expr.children(0).children(0).children(0);
+		val lex = id.lex;
+		
+	}
 	def main(args: Array[String]) : Unit = {
 		var ParseTree = new Node("ROOT", "");
 		ParseTree = ParseTreeBuilder.construct(ParseTree).children(0);
